48769623b488445f1b1e0bc52d92d8ea90d2cd5a
Travis CI <travis@travis-ci.org>
2018-07-20 22:54:00 +0000

    Travis build: 77

---
+++ versions/2/6/0/preview2/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "2.6.0-preview2";
+  version = [
+    "2"
+    "6"
+    "0"
+    "preview2"
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-2.6.0-preview2.tar.gz;
+  sha256 = "ee15ab35f17c942b1f41bd792f2494f639affff6e03babf44708b72fdbb6de34";
+}
