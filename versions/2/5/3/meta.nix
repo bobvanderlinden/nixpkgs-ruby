1accd5464570abadd209e36d41926d1cf1d1a639
Travis CI <travis@travis-ci.org>
2018-10-18 21:32:33 +0000

    Travis build: 167

---
+++ versions/2/5/3/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "2.5.3";
+  version = [
+    "2"
+    "5"
+    "3"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-2.5.3.tar.gz;
+  sha256 = "9828d03852c37c20fa333a0264f2490f07338576734d910ee3fd538c9520846c";
+}
