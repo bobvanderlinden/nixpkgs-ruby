5631ea3a35d404a20457317a1b92a36f1118d58b
Travis CI <travis@travis-ci.org>
2019-04-17 23:02:39 +0000

    Travis build: 348

---
+++ versions/2/6/3/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "2.6.3";
+  version = [
+    "2"
+    "6"
+    "3"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-2.6.3.tar.gz;
+  sha256 = "577fd3795f22b8d91c1d4e6733637b0394d4082db659fccf224c774a2b1c82fb";
+}
