b7aec4d126eea38358741aa12da1675c74923370
Travis CI <travis@travis-ci.org>
2019-08-28 00:09:32 +0000

    Travis build: 480

---
+++ versions/2/4/7/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "2.4.7";
+  version = [
+    "2"
+    "4"
+    "7"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-2.4.7.tar.gz;
+  sha256 = "a546dcd1491274c0dceb3acdb43f96d51049ece682226e12e8f1ccf0a9daebf1";
+}
---
+++ versions/2/5/6/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "2.5.6";
+  version = [
+    "2"
+    "5"
+    "6"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-2.5.6.tar.gz;
+  sha256 = "8829a09526a5b315714fd1eb1fa561f787979faa4953df3cc23d670870cfbdc8";
+}
