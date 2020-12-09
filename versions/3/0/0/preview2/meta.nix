b047cc3f294d3decae9dc05182f3325f59b6a781
Travis CI <travis@travis-ci.org>
2020-12-09 04:43:53 +0000

    Travis build: 949

---
+++ versions/3/0/0/preview2/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "3.0.0-preview2";
+  version = [
+    "3"
+    "0"
+    "0"
+    "preview2"
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-3.0.0-preview2.tar.gz;
+  sha256 = "9de8661565c2b1007d91a580e9a7e02d23f1e8fc8df371feb15a2727aa05fd9a";
+}
