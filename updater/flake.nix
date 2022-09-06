f5948fcd57d01acbdf4b5f6623fe1c12dcd2683e
Bob van der Linden <bobvanderlinden@gmail.com>
2022-09-06 18:04:03 +0200

    use nix for updater

---
+++ updater/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+  };
+  outputs = { dream2nix, ... }:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = [ "x86_64-linux" ];
+      config.projectRoot = ./.;
+      source = ./.;
+      settings = [
+        {
+          subsystemInfo.noDev = true;
+          subsystemInfo.nodejs = 14;
+        }
+      ];
+    };
+}
