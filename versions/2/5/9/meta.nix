1635400e635c83ee3f72b3e678a473a4df4646c1
Bob van der Linden <bobvanderlinden@gmail.com>
2022-11-24 23:48:03 +0100

    ruby-2.5.9: fix missing source url

--- versions/2/5/9/meta.nix
+++ versions/2/5/9/meta.nix
@@ -6,6 +6,6 @@
     "9"
     ""
   ];
-  url = https://cache.ruby-lang.org/pub/ruby/ruby-2.5.9.tar.gz;
+  url = https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.9.tar.gz;
   sha256 = "f5894e05f532b748c3347894a5efa42066fd11cc8d261d4d9788ff71da00be68";
 }
