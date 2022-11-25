b801d4176d6d47f20067ca02d066321d7dda9f13
bobvanderlinden <bobvanderlinden@users.noreply.github.com>
2022-11-25 05:38:58 +0000

    Update Ruby versions

---
+++ versions/3/0/5/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "3.0.5";
+  version = [
+    "3"
+    "0"
+    "5"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-3.0.5.tar.gz;
+  sha256 = "9afc6380a027a4fe1ae1a3e2eccb6b497b9c5ac0631c12ca56f9b7beb4848776";
+}
---
+++ versions/3/1/3/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "3.1.3";
+  version = [
+    "3"
+    "1"
+    "3"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-3.1.3.tar.gz;
+  sha256 = "5ea498a35f4cd15875200a52dde42b6eb179e1264e17d78732c3a57cd1c6ab9e";
+}
