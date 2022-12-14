cafeebe8abee0b483374da85218207bf77545fba
Will Leinweber <will@bitfission.com>
2022-12-14 19:01:53 +0100

    Inline rubygems patch to fix sha mismatch

--- lib/rubygems/3.0.nix
+++ lib/rubygems/3.0.nix
@@ -10,12 +10,7 @@ stdenv.mkDerivation {
     url = "http://production.cf.rubygems.org/rubygems/rubygems-3.2.33.tgz";
     hash = "sha256-bIQIzS4F3IdwwxdmH0jVnNKcrLzRji8K7V1LqoibkC0=";
   };
-  patches = [
-    (fetchurl {
-      url = "https://github.com/bobvanderlinden/rubygems/commit/0b5bcc8075deaedf692ac0d720ee03e0ca4dabc9.patch";
-      hash = "sha256-hCHOGztZB67vB3hJ6lKdsgiUBw4SdhLQpFZbxKEOSjI=";
-    })
-  ];
+  patches = [ ./3.0.patch ];
   installPhase = ''
     mkdir -p $out
     cp -r * $out/
