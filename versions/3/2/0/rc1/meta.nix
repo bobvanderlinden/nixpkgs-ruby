0f5aef14d2fe9d10b106cfb21122f9d0d40417ff
bobvanderlinden <bobvanderlinden@users.noreply.github.com>
2022-12-17 13:22:03 +0000

    Update Ruby versions

---
+++ versions/3/2/0/rc1/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "3.2.0-rc1";
+  version = [
+    "3"
+    "2"
+    "0"
+    "rc1"
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.0-rc1.tar.gz;
+  sha256 = "3bb9760c1ac1b66416aaa4899809f6ccd010e57038eaaeca19a383fd56275dac";
+}
