18e882439bd21294749a672e7b006470ce1d4cae
Bob van der Linden <bobvanderlinden@gmail.com>
2022-12-21 02:00:39 +0100

    rubygems: simplify overrides for <3

--- rubygems/overrides.nix
+++ rubygems/overrides.nix
@@ -4,22 +4,9 @@
 [
   {
     condition = version: with versionComparison version;
-      hasMajorMinor 2 6;
+      lessThan "3.0.0";
     override = pkg: pkg.overrideAttrs (final: prev: {
       dontPatchShebangs = true;
-      patches = prev.patches ++ [
-        (fetchpatch
-          {
-            url = "https://github.com/bobvanderlinden/rubygems/commit/0b5bcc8075deaedf692ac0d720ee03e0ca4dabc9.patch";
-            hash = "sha256-hCHOGztZB67vB3hJ6lKdsgiUBw4SdhLQpFZbxKEOSjI=";
-          })
-      ];
-    });
-  }
-  {
-    condition = version: with versionComparison version;
-      hasMajorMinor 2 7;
-    override = pkg: pkg.overrideAttrs (final: prev: {
       patches = prev.patches ++ [
         (fetchpatch {
           url = "https://github.com/zimbatm/rubygems/compare/v2.6.6...v2.6.6-nix.patch";
@@ -30,7 +17,7 @@
   }
   {
     condition = version: with versionComparison version;
-      hasMajor 3;
+      greaterOrEqualTo "3.0.0";
     override = pkg: pkg.overrideAttrs (final: prev: {
       patches = prev.patches ++ [
         (fetchpatch {
