dd9667380acbc66b88ef282a07b677d2d307d623
Bob van der Linden <bobvanderlinden@gmail.com>
2022-12-23 17:30:55 +0100

    rubygems: update hashes from ci

--- rubygems/package-fn.nix
+++ rubygems/package-fn.nix
@@ -12,15 +12,15 @@ stdenv.mkDerivation {
   patches = [
     (fetchpatch {
       url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0001-add-post-extract-hook.patch";
-      hash = "sha256-vC9D0iWJ+4HBhHZfivxLVjbwcYGA10P67cr9m6+WhaE=";
+      hash = "sha256-kHGcrBMTwixCs06mCPnpv3B3mVseT08izzJ8F7b3u+M=";
     })
     (fetchpatch {
       url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0002-binaries-with-env-shebang.patch";
-      hash = "sha256-TH5fMVdtwEFTDy6i3RjjkqViHwO3TExTs+Iqiok2KSc=";
+      hash = "sha256-DQVwqCRqkjtXNKcUi/363reFUgFsTOb328da3xGnfcY=";
     })
     (fetchpatch {
       url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0003-gem-install-default-to-user.patch";
-      hash = "sha256-WXDClowS398BKPnNm+fNf4OKVxlbYLLrVxVf1lL5v0A=";
+      hash = "sha256-8XR1FZ6LFgoNXl/65eeFGmJ8EeJUsVFmELHDAJxS61Q=";
     })
   ];
 
