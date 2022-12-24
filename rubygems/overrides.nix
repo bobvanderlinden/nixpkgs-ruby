7075c96a9095a90740129b8f59e2e5ef06a03893
Bob van der Linden <bobvanderlinden@gmail.com>
2022-12-25 00:16:43 +0100

    make ruby 3.1.3 use rubygems 3.4

--- rubygems/overrides.nix
+++ rubygems/overrides.nix
@@ -5,6 +5,26 @@
 [
   {
     condition = version: with versionComparison version;
+      lessThan "3.4.0";
+    override = pkg: pkg.overrideAttrs (final: prev: {
+      patches = [
+        (fetchpatch {
+          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0001-add-post-extract-hook.patch";
+          hash = "sha256-kHGcrBMTwixCs06mCPnpv3B3mVseT08izzJ8F7b3u+M=";
+        })
+        (fetchpatch {
+          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0002-binaries-with-env-shebang.patch";
+          hash = "sha256-DQVwqCRqkjtXNKcUi/363reFUgFsTOb328da3xGnfcY=";
+        })
+        (fetchpatch {
+          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0003-gem-install-default-to-user.patch";
+          hash = "sha256-8XR1FZ6LFgoNXl/65eeFGmJ8EeJUsVFmELHDAJxS61Q=";
+        })
+      ];
+    });
+  }
+  {
+    condition = version: with versionComparison version;
       lessThan "3.0.0";
     override = pkg: pkg.overrideAttrs (final: prev: {
       dontPatchShebangs = true;
--- rubygems/package-fn.nix
+++ rubygems/package-fn.nix
@@ -15,8 +15,8 @@ stdenv.mkDerivation {
       hash = "sha256-kHGcrBMTwixCs06mCPnpv3B3mVseT08izzJ8F7b3u+M=";
     })
     (fetchpatch {
-      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0002-binaries-with-env-shebang.patch";
-      hash = "sha256-DQVwqCRqkjtXNKcUi/363reFUgFsTOb328da3xGnfcY=";
+      url = "https://github.com/bobvanderlinden/rubygems/commit/f0189829b5b101ef200ac6f08b2e1c706eb48721.patch";
+      hash = "sha256-zS+1HQ5mZ+Qm0HAUYJq6BTKRhXfNc+ec8gsEn1tO8+E=";
     })
     (fetchpatch {
       url = "https://raw.githubusercontent.com/NixOS/nixpkgs/cc1bb678f4205ad1c4db34c20b4327cb2cc89a93/pkgs/development/interpreters/ruby/rubygems/0003-gem-install-default-to-user.patch";
