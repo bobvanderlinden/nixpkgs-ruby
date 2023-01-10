b2d421328e12cb33e0ac4cba69cc80b2bd3b7edd
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-10 16:57:05 +0100

    ruby: passthrough parsed version

--- ruby/package-fn.nix
+++ ruby/package-fn.nix
@@ -150,6 +150,7 @@ let
       };
 
       passthru = {
+        version = import ./parse-version.nix version;
         rubyEngine = "ruby";
         libPath = "lib/${self.passthru.rubyEngine}/${libDir}";
         gemPath = "lib/${self.passthru.rubyEngine}/gems/${libDir}";
