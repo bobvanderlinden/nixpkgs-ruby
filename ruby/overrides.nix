49cf3b46b8c1dab5660d860856203a95f80c3062
Bob van der Linden <bobvanderlinden@gmail.com>
2025-07-17 19:35:00 +0200

    Mark broken ruby versions as such

--- ruby/overrides.nix
+++ ruby/overrides.nix
@@ -13,24 +13,6 @@
         "2.1.0-preview2"
         "2.1.0-rc1"
         "2.2.0"
-        "2.2.10"
-        "2.3.0"
-        "2.3.1"
-        "2.3.2"
-        "2.3.3"
-        "2.3.4"
-        "2.4.0-preview1"
-        "2.4.8"
-        "2.5.2"
-        "2.5.8"
-      ];
-    override = pkg: pkg.overrideAttrs (finalAttrs: previousAttrs: { meta = previousAttrs.meta // { broken = true; }; });
-  }
-  # Some of the older versions do not build on OSX. Mark these as broken.
-  {
-    condition = version: stdenv.isDarwin && (with versionComparison version;
-      composeAny hasPrefix [
-        "2.1"
         "2.2.1"
         "2.2.2"
         "2.2.3"
@@ -40,7 +22,33 @@
         "2.2.7"
         "2.2.8"
         "2.2.9"
+        "2.2.10"
+        "2.3.0"
+        "2.3.1"
+        "2.3.2"
+        "2.3.3"
+        "2.3.4"
+        "2.3.5"
+        "2.3.6"
+        "2.3.7"
+        "2.3.8"
+        "2.4.0-preview1"
+        "2.4.8"
+        "2.5.2"
+        "2.5.8"
         "2.6.7"
+      ];
+    override = pkg: pkg.overrideAttrs (finalAttrs: previousAttrs: { meta = previousAttrs.meta // { broken = true; }; });
+  }
+  # Some of the older versions do not build on OSX. Mark these as broken.
+  {
+    condition = version: stdenv.isDarwin && (with versionComparison version;
+      composeAny hasPrefix [
+        "2.1"
+        "2.2.2"
+        "2.2.3"
+        "2.2.4"
+        "2.2.5"
         "3.1.0"
         "3.1.1"
         "3.1.2"
